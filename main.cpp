624de63f0e4f6c3292112a8e6e10eed590eb1a71
DarkFranki <71492094+DarkFranki@users.noreply.github.com>
2020-10-14 16:41:29 +0300

    opana

--- main.cpp
+++ main.cpp
@@ -1,23 +1,105 @@
 #include <iostream>
 #include <string>
+
+#include<stdlib.h>
 using namespace std;
+
+
+
+
+void Foo1(int *arr);			//increase by 1
+void Foo2(int *arr);			//swap the elements of arr
+void Foo3(int *arr);			//print revert
+void Foo4(int *arr);			//min value
+void Foo5(int *arr);			//bubble sort
+
+
+void quickSortR(int* arr, 		//quick sort
+int SIZE); 
+
+
+
+
+
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+Node *head, *tail;
+
+void PrintStruct()
+{
+	
+	Node *tmp = head;
+	
+	while(tmp != NULL)
+	{
+		cout << tmp -> data << "\t";
+		
+		tmp = tmp -> next;
+	}
+	
+	cout << endl << endl;
+}
+
+void AddTail()
+{
+	
+	int a;
+	
+	Node *Four = new Node();
+	
+	cout << "set the data: ";
+	
+	cin >> a;
+	Four -> data = a;
+	
+	tail -> next = Four;
+	Four -> next = NULL;
+	tail = Four;
+	
+}
+
 
 
 int main()
  {
- 	int arr[5] = {3,5,7,6,9};
+ 	Node *First = new Node();
+ 	Node *Second = new Node();
+ 	Node *Third = new Node();
+ 	
+ 	
+ 	First -> data = 10;
+ 	First -> next = Second;
+ 	
+ 	Second -> data = 15;
+ 	Second -> next = Third;
+ 	
+ 	Third -> data = 20;
+ 	Third -> next = NULL;
+ 	
+ 	head = First;
+ 	tail = Third;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	int arr[5] = {9,6,2,3,4};
  	
  	int command;
  	
  	int value;
  	int indx;
  	
- 	setlocale(0, "");
- 	
- 	
  	while (true)			
 {
-	cout << "0 - IDDQD) 3 - OOOOOOOOOO MY DEFEND" << endl <<  "QWRTTDW:";
+	cout << "0 - Exit\n1 - Read\n2 - Write(not more than 5 elements)\n3 - increase by 1\n4 - swap elem arr\n5 - print arr rev\n6 - min val\n7 - bubble sort\n8 - quick sort" << endl << endl <<  "please, choose a command: ";
 	cin>>command;
 	system("cls");
 	
@@ -33,7 +115,7 @@ int main()
 		break;
 			
 		case 2:
-			cout<<"RGGBVRRET:";
+			cout<<"Select the index of the element to fill and its value:";
 			cin>>indx>>value;
 			
 			arr[indx] = value;
@@ -41,8 +123,43 @@ int main()
 		break;
 		
 		case 3:
-		
+			Foo1(arr);
 		break;	
+		
+		case 4:
+			Foo2(arr);
+		break;
+		
+		case 5:
+			Foo3(arr);
+		break;
+		
+		case 6:
+			Foo4(arr);
+		break;
+		
+		case 7:
+			Foo5(arr);
+		break;
+		
+		case 8:
+			quickSortR(arr, 5);
+		
+		case 9:
+			 for(int i = 0; i < 5; i++)
+			{
+				cout << arr[i];
+			}			
+			cout << endl << endl;
+		break;
+		
+		case 10:
+			PrintStruct();
+		break;
+		
+		case 11:
+			AddTail();
+		break;
 		
 		case 0:
 			return 0;
@@ -53,3 +170,4 @@ int main()
  	
 	return 0;
  }
+
