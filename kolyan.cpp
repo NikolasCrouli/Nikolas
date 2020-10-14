43e6e518a203171b94851ba35fff594a84eb2a58
DarkFranki <71492094+DarkFranki@users.noreply.github.com>
2020-10-14 15:57:48 +0300

    Boba

--- kolyan.cpp
+++ kolyan.cpp
@@ -7,4 +7,73 @@ using namespace std;
 
 
 
-void Foo1(int *arr)
+void Foo1(int *arr);
+void Foo2(int *arr);
+void Foo3(int *arr);
+void Foo4(int *arr);
+void foo5(int *arr);
+
+
+void quickSortR(int* arr,
+int SIZE);
+
+
+
+
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+Node *head, *tail;
+
+void PrintStruct()
+{
+	
+	Node *tmp = head;
+	
+	while(tmp != NULL)
+	{
+		cout << tmp -> data << "\t";
+		
+		tmp = tmp -> next;
+	}
+	cout << endl << endl;
+}
+
+void AddTail()
+{
+	
+	int a;
+	
+	Node *Four = new Node();
+	
+	cout << "set the data: ";
+	
+	cin >> a;
+	Four -> data = a;
+	
+	tail -> next = Four;
+	Four -> next = NULL;
+	tail = Four;
+	
+}
+
+
+int main()
+{
+	Node *First = New Node();
+	Node *Second = new Node();
+	Node *Third = new Node();
+	
+	
+	First -> data = 10;
+	First -> next = Second;
+	
+	Second -> data = 15;
+	Second -> next = Third;
+	
+	
+	
+}
